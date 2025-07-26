12b411a0b50494ff4b9709f4a5c534a29d7a22f0
3058509009 <3058509009@qq.com>
2025-07-26 23:35:10 +0800

    feat(product): 添加商品管理功能- 新增商品实体类、控制器、服务接口及其实现类、自定义异常处理等 -集成 MyBatis Plus框架实现数据库操作 - 添加商品创建、更新、删除、详情查看和分页查询等功能- 优化了错误处理，增加了商品不存在等自定义异常

--- seckill-common/src/main/java/exception/BizException.java
+++ seckill-common/src/main/java/exception/BizException.java
@@ -1,7 +1,7 @@
 package exception;
 
 public class BizException extends RuntimeException {
-    private final int code;
+    private final ErrorCode code;
 
     public BizException(ErrorCode code, String message) {
         super(message);
